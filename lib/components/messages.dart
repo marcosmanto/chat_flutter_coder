8b93082f0ba8183a356201525d67360c525cc897
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-10 00:06:05 -0300

    Added ChatService in Messages component.

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,3 +1,5 @@
+import 'package:chat_flutter_coder/core/models/chat_message.dart';
+import 'package:chat_flutter_coder/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -5,8 +7,26 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Mensagens'),
+    return StreamBuilder<List<ChatMessage>>(
+      stream: ChatService().messagesStream(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(
+            child: Text('Sem dados. Inicie uma conversa.'),
+          );
+        } else {
+          final msgs = snapshot.data!;
+          return ListView.builder(
+            reverse: true,
+            itemCount: msgs.length,
+            itemBuilder: (context, index) => Text(msgs[index].text),
+          );
+        }
+      },
     );
   }
 }

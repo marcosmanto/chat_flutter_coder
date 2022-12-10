7ba59bde20c99901b5f8ea15a357b9256f4e3c58
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-09 22:57:48 -0300

    Added NewMessage and Messages components.

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,6 +1,7 @@
+import 'package:chat_flutter_coder/components/messages.dart';
+import 'package:chat_flutter_coder/components/new_message.dart';
+import 'package:chat_flutter_coder/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
-
-import '../core/services/auth/auth_service.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -8,19 +9,48 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Chat Page'),
-            TextButton(
-                onPressed: () {
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Chat',
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            DropdownButton(
+              iconEnabledColor: Colors.white,
+              underline: Container(
+                height: 0,
+              ),
+              icon: const Icon(Icons.more_vert),
+              items: [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: SizedBox(
+                    width: 70,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Icon(Icons.exit_to_app),
+                        Text('Sair'),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+              onChanged: (value) {
+                if (value == 'logout') {
                   AuthService().logout();
-                },
-                child: const Text('Sair'))
+                }
+              },
+            )
           ],
         ),
-      ),
-    );
+        body: SafeArea(
+            child: Column(
+          children: const [
+            Expanded(child: Messages()),
+            NewMessage(),
+          ],
+        )));
   }
 }

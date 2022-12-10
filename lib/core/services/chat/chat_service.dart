9cdaca58c323b21fbb66ecd60f1693666ed46d4b
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-09 23:52:19 -0300

    Created ChatService ans it's respective mock.

---
+++ lib/core/services/chat/chat_service.dart
@@ -0,0 +1,11 @@
+import 'package:chat_flutter_coder/core/models/chat_message.dart';
+import 'package:chat_flutter_coder/core/models/chat_user.dart';
+import 'package:chat_flutter_coder/core/services/chat/chat_mock_service.dart';
+
+abstract class ChatService {
+  Stream<List<ChatMessage>> messagesStream();
+  Future<ChatMessage> save(String text, ChatUser user);
+  factory ChatService() {
+    return ChatMockService();
+  }
+}

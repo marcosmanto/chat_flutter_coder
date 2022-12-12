5bfa63ae1ef992d8f4c629cfa0b596c1b13f52c0
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-12 20:35:45 -0300

    Started push notification service.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:chat_flutter_coder/core/services/notifications/push_notification_service.dart';
 import 'package:chat_flutter_coder/pages/auth_or_app_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,18 +12,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData().copyWith(
-        appBarTheme: const AppBarTheme(foregroundColor: Color(0xFF05445E)),
-        colorScheme: ThemeData().colorScheme.copyWith(
-              primary: const Color(0xFF05445E),
-              secondary: const Color(0xFFA0E7E5),
-              tertiary: const Color(0xFFFBE7C6),
-            ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => PushNotificationService(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData().copyWith(
+          appBarTheme: const AppBarTheme(foregroundColor: Color(0xFF05445E)),
+          colorScheme: ThemeData().colorScheme.copyWith(
+                primary: const Color(0xFF05445E),
+                secondary: const Color(0xFFA0E7E5),
+                tertiary: const Color(0xFFFBE7C6),
+              ),
+        ),
+        home: const AuthOrAppPage(),
       ),
-      home: const AuthOrAppPage(),
     );
   }
 }

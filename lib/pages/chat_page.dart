7fea6026b0dbdbc431bceaa1d356ee87e445fe29
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-12 23:27:10 -0300

    Added messages to notification page.

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:chat_flutter_coder/components/messages.dart';
 import 'package:chat_flutter_coder/components/new_message.dart';
+import 'package:chat_flutter_coder/core/models/chat_notification.dart';
 import 'package:chat_flutter_coder/core/services/auth/auth_service.dart';
 import 'package:chat_flutter_coder/core/services/notifications/push_notification_service.dart';
 import 'package:chat_flutter_coder/pages/notification_page.dart';
@@ -12,79 +15,90 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          iconTheme: const IconThemeData(color: Colors.white),
-          title: const Text(
-            'Chat',
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: [
-            DropdownButtonHideUnderline(
-              child: DropdownButton(
-                iconEnabledColor: Colors.white,
-                underline: Container(
-                  height: 0,
-                ),
-                icon: const Icon(Icons.more_vert),
-                items: [
-                  DropdownMenuItem(
-                    value: 'logout',
-                    child: SizedBox(
-                      width: 70,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Icon(Icons.exit_to_app),
-                          Text('Sair'),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-                onChanged: (value) {
-                  if (value == 'logout') {
-                    AuthService().logout();
-                  }
-                },
+      appBar: AppBar(
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text(
+          'Chat',
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
+              iconEnabledColor: Colors.white,
+              underline: Container(
+                height: 0,
               ),
-            ),
-            Stack(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const NotificationPage(),
-                      ),
-                    );
-                  },
-                  icon: const Icon(Icons.notifications),
-                ),
-                Positioned(
-                  top: 5,
-                  right: 5,
-                  child: CircleAvatar(
-                    backgroundColor: Colors.red[800],
-                    maxRadius: 10,
-                    child: Text(
-                      '${Provider.of<PushNotificationService>(context).itemCount}',
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                          fontSize: 12, fontWeight: FontWeight.bold),
+              icon: const Icon(Icons.more_vert),
+              items: [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: SizedBox(
+                    width: 70,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Icon(Icons.exit_to_app),
+                        Text('Sair'),
+                      ],
                     ),
                   ),
                 )
               ],
-            )
-          ],
+              onChanged: (value) {
+                if (value == 'logout') {
+                  AuthService().logout();
+                }
+              },
+            ),
+          ),
+          Stack(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const NotificationPage(),
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.notifications),
+              ),
+              Positioned(
+                top: 5,
+                right: 5,
+                child: CircleAvatar(
+                  backgroundColor: Colors.red[800],
+                  maxRadius: 10,
+                  child: Text(
+                    '${Provider.of<PushNotificationService>(context).itemCount}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+      body: SafeArea(
+          child: Column(
+        children: const [
+          Expanded(child: Messages()),
+          NewMessage(),
+        ],
+      )),
+      /*floatingActionButton: FloatingActionButton(
+        onPressed: () =>
+            Provider.of<PushNotificationService>(context, listen: false).add(
+          ChatNotification(
+            title: 'Mais uma notificação',
+            body: Random().nextDouble().toString(),
+          ),
         ),
-        body: SafeArea(
-            child: Column(
-          children: const [
-            Expanded(child: Messages()),
-            NewMessage(),
-          ],
-        )));
+        child: const Icon(Icons.add),
+      ),*/
+    );
   }
 }
--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -1,10 +1,15 @@
+import 'package:chat_flutter_coder/core/services/notifications/push_notification_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final PushNotificationService service = Provider.of(context);
+    final items = service.items;
+    final itemCount = service.itemCount;
     return Scaffold(
       appBar: AppBar(
         iconTheme: const IconThemeData(color: Colors.white),
@@ -14,9 +19,25 @@ class NotificationPage extends StatelessWidget {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: const Center(
-        child: Text('Notificações'),
-      ),
+      body: itemCount > 0
+          ? ListView.separated(
+              itemCount: service.itemCount,
+              separatorBuilder: (BuildContext context, int index) {
+                return const Divider();
+              },
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  trailing: Icon(
+                    Icons.remove_circle_sharp,
+                    color: Colors.red[800],
+                  ),
+                  title: Text(items[index].title),
+                  subtitle: Text(items[index].body),
+                  onTap: () => service.remove(index),
+                );
+              },
+            )
+          : const Center(child: Text('Não existem mensagens para você.')),
     );
   }
 }
